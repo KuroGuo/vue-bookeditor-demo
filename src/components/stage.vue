9ffdc98622db32145c5a9b1d7b6d3cf5b747559c
KuroGuo <413298956@qq.com>
2015-04-28 18:40:22 +0800

    fix page-navigitor

--- src/components/stage.vue
+++ src/components/stage.vue
@@ -1,16 +1,19 @@
 <style>
+.stage {
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+
 .page {
   box-shadow: 0 1px 6px #aaa;
   position: relative;
-  margin-top: 10px;
-  margin-bottom: 10px;
   -webkit-transform-origin: left top;
           transform-origin: left top;
 }
 </style>
 
 <template>
-<div class="stage">
+<div class="stage" v-style="width: page.width * scaling + 'px', height: page.height * scaling + 'px'">
   <div class="page" v-style="width: page.width + 'px', height: page.height + 'px', transform: 'scale(' + scaling + ')'">
     <div v-component="{{box.type}}" v-repeat="box: page.boxes" v-with="displayScaling: scaling"></div>
   </div>
@@ -19,6 +22,7 @@
 
 <script>
 module.exports = {
+  replace: true,
   data: function () {
     return {
       scaling: 0.45
